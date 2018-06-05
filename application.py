196c3cf0c0d70a3a641a6c034bf158335923f969
Chinzo Dandar <chinzo.dandar@gmail.com>
2018-06-05 14:20:41 -0700

    eb configuration

--- application.py
+++ application.py
@@ -1,12 +1,10 @@
 from app import create_app
-# # from vendor import app
-#
+
+
 application = create_app()
-application.run(debug=True)
-#
-# # app.run(host='0.0.0.0', port=5001, debug=True)
-#
-# if __name__ == '__main__':
-    # app.run(host='0.0.0.0')
-    # app.run(debug=True)
-# # app.run(host='0.0.0.0', port=5001, debug=True)
+
+if __name__ == '__main__':
+    application.debug = True
+    application.run()
+
+# app.run(host='0.0.0.0', port=5001, debug=True)
