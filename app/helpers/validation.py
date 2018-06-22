6d9adc86ebb60f39d1c00c11df3c42b81de32b94
chinzo <ChinzoD@users.noreply.github.com>
2018-06-21 18:28:05 -0700

    config from mac2

--- app/helpers/validation.py
+++ app/helpers/validation.py
@@ -311,7 +311,8 @@ def excel_validation(request, form):
                 catalog_dict.append(
                     dict(field_name=headers[index], type='optional', value=value, history_id=history.id))
     print("ttt line 304")
-    print("celery: {}".format(get_location.delay()))
+    catalog_dict = []
+    print("celery: {}".format(get_location.delay(catalog_dict)))
     # try:
     #     print("t1")
     #     # store_in_dynamo(catalog_dict)
--- app/tasks.py
+++ app/tasks.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from app.data.models.catalog import CatalogModel
 
 # Be sure to add your SQS URL below!
 #broker='sqs://AKIAJOX2FI6TLU6VKXSA:jGvsUp1FVBW+O46ZbRT5lHAP4fsL8OyBUix5SJaX@'
@@ -23,11 +24,16 @@ application = Celery('tasks', broker='sqs://AKIAJOX2FI6TLU6VKXSA:jGvsUp1FVBW+O46
 
 
 @application.task(name='tasks.get_location')
-def get_location():
+def get_location(catalog_dict):
+        print("h1")
+        print(catalog_dict)
+        catalog = CatalogModel('celery', 'celery', 'celery', 804)
+        catalog.save_to_db()
+        print("h2")
         # Get the location from the API
         # r = requests.get('http://freegeoip.net/json/' + address)
         # jstr = str(r.json()['latitude']) + ',' + str(r.json()['longitude'])
         # # Update the ElasticSearch index
         # resp = client.make_request(method='POST', path='/big_survey/quiz/' + user + '/_update',
         #                            data='{"doc":{"geo":"' + jstr + '"}}')
-        return "hi"
+        return "return hi"
