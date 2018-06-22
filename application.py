3604f32ce0b725a39eb5d30bcf6677b68382cf98
chinzo <ChinzoD@users.noreply.github.com>
2018-06-22 14:27:03 -0700

    config

--- application.py
+++ application.py
@@ -32,8 +32,8 @@ application = create_app()
 #     print("test2")
 #     return "test"
 #
-# if __name__ == '__main__':
-#     application.debug = True
-#     application.run()
+if __name__ == '__main__':
+    application.debug = True
+    application.run()
 
 # app.run(host='0.0.0.0', port=5001, debug=True)
