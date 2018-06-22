f6567eb099e98ea3b8bb5955aa1c5e2e8e6fabf5
chinzo <ChinzoD@users.noreply.github.com>
2018-06-22 14:46:25 -0700

    clerey test from application.py

--- application.py
+++ application.py
@@ -1,37 +1,47 @@
 from app import create_app
 
-# # BEGIN CELERY
-# from celery import Celery
-#
-#
-# def make_celery(application):
-#     # celery = Celery(application.import_name, broker=application.config['CELERY_BROKER_URL'])
-#     celery = Celery("flask-es", broker=application.config['CELERY_BROKER_URL'])
-#     celery.conf.update(application.config)
-#     celery.conf.broker_transport_options = {'region': 'us-west-1'}
-#     TaskBase = celery.Task
-#     class ContextTask(TaskBase):
-#         abstract = True
-#         def __call__(self, *args, **kwargs):
-#             with application.app_context():
-#                 return TaskBase.__call__(self, *args, **kwargs)
-#     celery.Task = ContextTask
-#     return celery
-# # DONE CELERY
+# BEGIN CELERY
+from celery import Celery
+
+
+def make_celery(application):
+    # celery = Celery(application.import_name, broker=application.config['CELERY_BROKER_URL'])
+    celery = Celery("flask-es", broker=application.config['CELERY_BROKER_URL'])
+    celery.conf.update(application.config)
+    celery.conf.broker_transport_options = {'region': 'us-west-1'}
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with application.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+# DONE CELERY
 
 application = create_app()
 
-# celery = make_celery(application)
-#
-#
-#
-# @application.route('/test', methods=['GET', 'POST'])
-# def take_test():
-#     print("test1")
-#     get_location.delay("")
-#     print("test2")
-#     return "test"
-#
+celery = make_celery(application)
+
+
+@application.route('/test', methods=['GET', 'POST'])
+def take_test():
+    print("test1")
+    get_location.delay("")
+    print("test2")
+    return "test"
+
+#Celery Task
+@celery.task(name='tasks.get_location', queue="flask-es")
+def get_location(user):
+        # Get the location from the API
+        print("hi from celery")
+        from app.data.models.catalog import CatalogModel
+        catalog = CatalogModel('celery', 'celery', 'celery', 47)
+        catalog.save_to_db()
+        return
+#End Task
+
 if __name__ == '__main__':
     application.debug = True
     application.run()
