3709bbf6dfd6bdd1057e4d31423c1897bb3adcc4
Khanh Tang <khtang@gimel.cluster.ucsf.bkslab.org>
2019-06-28 10:31:57 -0700

    revert to fix the api

--- application.py
+++ application.py
@@ -3,7 +3,6 @@ from app import create_app
 application = create_app()
 
 if __name__ == '__main__':
-    application.debug = True
-    application.run()
-
-# app.run(host='0.0.0.0', port=5001, debug=True)
+    #application.debug = True
+    #application.run()
+    application.run(host='0.0.0.0', port=5020, debug=True)
